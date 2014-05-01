52100b3629ed46126ffb216f37783db25cf7c292
Daniel Cruz <daniel.cruz@4thsource.com>
2014-04-30 19:44:07 -0500

    Project created

---
+++ bebeclick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bebeclick.Startup))]
+namespace bebeclick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
